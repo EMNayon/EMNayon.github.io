3e9d1cbd018a45c00a1aedf12d762f4b5c4dc6c2
EM Nayon <59680846+EMNayon@users.noreply.github.com>
2023-10-03 00:57:08 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
               {/* <p className="text-zinc-200 text-2xl font-apparel">Hi, My name is</p> */}
               <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=900&size=20&pause=1000&vCenter=true&width=435&height=60&lines=Hi,+My+Name+is;I'm+a" alt="Typing SVG" />
 
-             <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=900&size=40&pause=1000&vCenter=true&width=435&height=60&lines=EM+Nayon;Frontend+Developer" alt="Typing SVG" />
-              <p className="text-[#8892b0] py-4 max-w-[700px] font-thin">Welcome to my portfolio, where expertise in frontend development meets creativity and innovation. I am an expert frontend developer with a passion for crafting remarkable user interfaces that leave a lasting impression.</p>
+             <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=900&size=40&pause=1000&vCenter=true&width=435&height=60&lines=EM+Nayon;Web+Developer" alt="Typing SVG" />
+              <p className="text-[#8892b0] py-4 max-w-[700px] font-thin">Welcome to my portfolio, where expertise in web development meets creativity and innovation. I am an expert web developer with a passion for crafting remarkable user interfaces that leave a lasting impression.</p>
               <p className="text-[#8892b0] py-4 max-w-[700px] font-thin">Collaboration is at the heart of my approach. I thrive on working closely with designers, UX specialists, and backend developers to transform ideas into tangible realities</p>
               <p className="text-[#8892b0] py-4 max-w-[700px] font-thin">I invite you to explore my portfolio and witness the diverse range of projects I have worked on</p>
             </div>  
