58afefff5113fc8399eb89cf4df01457549256b7
EM Nayon <59680846+EMNayon@users.noreply.github.com>
2023-10-03 01:02:01 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="../assets/resume/emnayon_resume.pdf"
+              href="../assets/resume/em_nayon_cv.pdf"
               download={true}
             >
               Resume <BsFillPersonLinesFill size={30} />
